36955f31329d11a9fe15cf81830efbdb3e859e03
Morgan Goeller <morgango@gmail.com>
2023-10-30 16:09:49 -0500

    Ready for class

--- scratch.py
+++
@@ -1,9 +0,0 @@
-    import openai
-
-    # Send the messages to OpenAI and get the response
-    response = openai.ChatCompletion.create(
-        model='gpt-4',
-        messages=[],
-        temperature=temperature,
-        max_tokens=max_tokens,
-    )
